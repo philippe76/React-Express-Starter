ea9b4da311b97f1fd59b651f046e7e23f3897da5
Philippe_Lanougadere <p.lanougadere@gmail.com>
2020-08-29 23:06:04 +0200

    add dotenv variable

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+require("dotenv").config();
 
 const app = express();
 
@@ -8,7 +9,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 mongoose.connect(
-  "mongodb+srv://philippe:mossoko0302@cluster0.1fftp.mongodb.net/MERN-testDB",
+  process.env.DATABASE,
   { useUnifiedTopology: true, useNewUrlParser: true },
   () => console.log("connected to mongoDB")
 );
